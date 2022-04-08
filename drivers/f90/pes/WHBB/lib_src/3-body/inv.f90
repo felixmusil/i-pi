2527d45d25df5ddf555fcb6c0d7b6faef7e2362f
felixmusil <felix.musil@epfl.ch>
2022-04-08 13:51:28 +0200

    add whbb pot

---
+++ drivers/f90/pes/WHBB/lib_src/3-body/inv.f90
@@ -0,0 +1,9 @@
+MODULE inv
+!..use and access
+use inv_wp
+use inv_mgx
+use inv_mg63
+use inv_cxx
+use inv_cx63
+public
+END MODULE inv
---
+++ drivers/f90/pes/WHBB/lib_src/3-body/pes.f90
@@ -0,0 +1,8 @@
+MODULE pes
+use inv
+use pes0
+use pes_x6y3
+!use pes1_xyz
+use pes1c
+public
+END MODULE pes
